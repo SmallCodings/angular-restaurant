2a41ca5838b3865f992bbc840f63336c2b220215
Meece <smallcoding93@gmail.com>
2022-04-27 08:40:18 +0200

    Added navbar, banner, and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './content/home/home.component';
+import { MenuComponent } from './content/menu/menu.component';
+import { AboutComponent } from './content/about/about.component';
+import { ContactComponent } from './content/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
