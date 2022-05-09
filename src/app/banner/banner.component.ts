5e5dc23b50a230f3b1bb94ed03a3760ad5888c6a
Meece <smallcoding93@gmail.com>
2022-05-09 12:39:00 +0200

    Added banner navigation

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
+  bannerNumber: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onNextBanner(direction: number) {
+    this.bannerNumber += direction;
+    if (this.bannerNumber <= 0) {
+      this.bannerNumber = 3;
+    }
+    if (this.bannerNumber >= 4) {
+      this.bannerNumber = 1;
+    }
   }
 
 }
